931b57a52b998d24beb4def8796d836a6c07d1df
Robert Lee Brackett III <robert.brackett3@gmail.com>
2015-01-06 11:22:08 -0500

    Update replies_controller.rb

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,12 +1,27 @@
 class RepliesController < ApplicationController
 
 	def new
+		@user = User.find(params[:user_id])
+		@topic = Topic.find(params[:topic_id])
+		@reply = Reply.new
 	end
 
 	def edit
 	end
 
 	def create
+		@user = User.find(params[:user_id])
+		@topic = Topic.find(params[:topic_id])
+		@reply = @topic.replies.new(reply_params)
+
+		if @topic.save
+			@reply.user = @user #set the user on the reply, could also work for topic
+			flash[:notice] = "Reply saved successfully!"
+			redirect_to user_topic_path(@user, @topic)
+		else
+			flash[:alert] = "Something didn't work"
+			render 'new'
+		end
 	end
 
 	def update
